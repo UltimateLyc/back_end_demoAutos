a8d2593327e8f82a883ba66fff0ccd175b4202cf
Rafael Ramos <ingrnrg@gmail.com>
2023-02-02 20:35:17 -0600

    added models and functions in controllers

--- back_end/controllers/autosControllers.js
+++ back_end/controllers/autosControllers.js
@@ -1,20 +1,63 @@
 /* Llamado de dependencias */
 const asyncHandler = require('express-async-handler')
 
+/* Exportacion de modelos */
+
+const auto = require('../models/autoModel')
+
+/* Funciones para endpoints */
+
+// Funcion get
 const getAutos = asyncHandler (async (req, res) => {
-    res.status(200).json('Autos obtenidos')
+    const autos = await auto.find()
+    res.status(200).json(autos)
 })
 
+// Funcion post
 const setAuto = asyncHandler (async (req, res) => {
-    res.status(201).json('Auto agregado')
+    if(!req.body.brand || !req.body.model || !req.body.year || !req.body.color ) {
+        res.status(400)
+        throw new Error ('Por favor ingrese todos los datos solicitados')
+    }
+
+    const nuevoAuto =  await auto.create({
+        brand: req.body.brand,
+        model: req.body.model,
+        year: req.body.year,
+        color: req.body.color
+    })
+
+    res.status(201).json(nuevoAuto)
 })
 
+// Funcion put
 const updateAuto = asyncHandler (async (req, res) => {
-    res.status(200).json('Auto modificado')
+
+    const modAuto = await auto.findById(req.params.id)
+
+    if(!modAuto) {
+        res.status(400)
+        throw new Error('Car not found')
+    }
+
+    const updatedAuto = await auto.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(updatedAuto)
 })
 
+// Funcion delete
 const deleteAuto = asyncHandler (async (req, res) => {
-    res.status(200).json('Auto Eliminado')
+
+    const delAuto = await auto.findById(req.params.id)
+
+    if(!delAuto) {
+        res.status(400)
+        throw new Error('Car not found')
+    }
+
+    await delAuto.remove()
+
+    res.status(200).json(delAuto)
 })
 
 module.exports = {
---
+++ back_end/models/autoModel.js
@@ -0,0 +1,26 @@
+/* Llamado de dependencias */
+const mongoose = require('mongoose')
+
+/* Schemas */ 
+const  autoSchema = mongoose.Schema({
+    brand: {
+        type: String,
+        required: [true, ' Favor de ingresar la marca']
+    }, 
+    model: {
+        type: String,
+        required: [true, ' Favor de ingresar un modelo']
+    }, 
+    year: {
+        type: Number,
+        required: [true, ' Favor de ingresar un año']
+    }, 
+    color: {
+        type: String,
+        required: [true, ' Favor de ingresar un color']
+    }
+}, {
+    timestamps: true // Nos crea la fecha y hora de cuando fue creado y otra de cada que se modifique
+})
+
+module.exports = mongoose.model('AUTOS', autoSchema)
